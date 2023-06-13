68998a7588505e73815df1e2409588d37d1460c7
shahzaib <shahzi113awan@gmail.com>
2023-06-13 21:11:54 +0500

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 export type KeyMap = [string, string][]; // Array of ["oldKey", "newKey"] mappings
 
-export function updateKeyMap<T>(obj: Record<string, unknown>, map: Map<string, string> | KeyMap): T {
+export function updateKeyMap<T>(
+  obj: Record<string, unknown>,
+  map: Map<string, string> | KeyMap
+): T {
   const updatedObj = { ...obj };
 
-  function updateKeyRecursively(target: Record<string, unknown>, keyMap: KeyMap) {
+  function updateKeyRecursively(
+    target: Record<string, unknown>,
+    keyMap: KeyMap
+  ) {
     for (const [oldKey, newKey] of keyMap) {
       if (oldKey in target) {
         const value = target[oldKey];
@@ -11,56 +17,42 @@ export function updateKeyMap<T>(obj: Record<string, unknown>, map: Map<string, s
         target[newKey] = value;
       }
     }
-
     for (const key in target) {
       const value = target[key];
-      if (typeof value === 'object' && value !== null) {
-        if (Array.isArray(value)) {
-          for (const item of value) {
-            if (typeof item === 'object' && item !== null) {
-              const nestedKeyMap = keyMap.filter(([oldKey]) => oldKey.startsWith(key + '.')).map(([oldKey, newKey]) => {
-                const newKeyWithoutParent = newKey.substring(newKey.indexOf('.') + 1);
-                return [oldKey.substring(key.length + 1), newKeyWithoutParent];
-              }) as KeyMap;
-              updateKeyRecursively(item, nestedKeyMap);
-            }
-          }
-        } else {
-          const nestedKeyMap = keyMap.filter(([oldKey]) => oldKey.startsWith(key + '.')).map(([oldKey, newKey]) => {
-            const newKeyWithoutParent = newKey.substring(newKey.indexOf('.') + 1);
+      if (typeof value === "object" && value !== null) {
+        const nestedKeyMap = keyMap
+          .filter(([oldKey]) => oldKey.startsWith(key + "."))
+          .map(([oldKey, newKey]) => {
+            const newKeyWithoutParent = newKey.substring(
+              newKey.indexOf(".") + 1
+            );
             return [oldKey.substring(key.length + 1), newKeyWithoutParent];
           }) as KeyMap;
-          console.log(nestedKeyMap,value)
-          updateKeyRecursively(value as Record<string, unknown>, nestedKeyMap);
-        }
+        updateKeyRecursively(value as Record<string, unknown>, nestedKeyMap);
       }
     }
   }
-
-  const keyMap: KeyMap = Array.isArray(map) ? map : Array.from(map.entries());
+  const keyMap: KeyMap =  map as KeyMap
   updateKeyRecursively(updatedObj, keyMap);
 
   return updatedObj as T;
 }
 
-
-
-
 const obj = {
-  name: 'John',
+  name: "John",
   age: 25,
   address: {
-    street: '123 Main St',
-    city: 'New York',
-    country: 'USA',
-    name:"jaja",
-  }
+    street: "123 Main St",
+    city: "New York",
+    country: "USA",
+    name: "jaja",
+  },
 };
 
 const keyMap: KeyMap = [
-  ['name', 'firstName'],
-  ['address.name', 'address.lala'],
-  ['address.street','address.akak']
+  ["name", "firstName"],
+  ["address.name", "address.lala"],
+  ["address.street", "address.akak"],
 ];
 
 const updatedObj = updateKeyMap(obj, keyMap);
--- tests/unit.spec.ts
+++ tests/unit.spec.ts
@@ -55,6 +55,37 @@ describe('updateKeyMap', () => {
       expect(result).toEqual(obj);
     });
   
-     
+     test('should handle an array as the key mapping', () => {
+  const obj = {
+    age: 25,
+    address: {
+      city: 'New York',
+      country: 'USA',
+      name: 'jaja',
+      'address.line1': '123 Main St'
+    },
+    firstName: 'John'
+  };
+
+  const keyMap: KeyMap = [
+    ['address.city', 'address.cityName'],
+    ['address.name', 'address.firstName'],
+    ['address.address.line1', 'address.line1']
+  ];
+
+  const result = updateKeyMap(obj, keyMap);
+
+  expect(result).toEqual({
+    age: 25,
+    address: {
+      cityName: 'New York',
+      country: 'USA',
+      firstName: 'jaja',
+      line1: '123 Main St'
+    },
+    firstName: 'John'
+  });
+});
+
   });
   
