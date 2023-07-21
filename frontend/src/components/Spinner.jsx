f475454b388187747eb41fb2e554bf88b9d01693
Aamir Cheema <amircheema295@gmail.com>
2023-07-21 08:56:12 -0700

    Frontend authentication

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='loadingSpinnerConatiner'>
+      <div className='loadingSpinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
